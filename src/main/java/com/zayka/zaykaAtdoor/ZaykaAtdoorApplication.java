c18b03f8c3c39d70485c37e25bdd72a9ce46f403
haru-kunyo <dainxin@gmail.com>
2025-06-07 17:55:28 +0530

    Initial commit

--- src/main/java/com/zayka/zaykaAtdoor/ZaykaAtdoorApplication.java
+++ src/main/java/com/zayka/zaykaAtdoor/ZaykaAtdoorApplication.java
@@ -3,10 +3,17 @@ package com.zayka.zaykaAtdoor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class ZaykaAtdoorApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(ZaykaAtdoorApplication.class, args);
 	}
 
--- src/main/java/com/zayka/zaykaAtdoor/config/TwilioConfiguration.java
+++ src/main/java/com/zayka/zaykaAtdoor/config/TwilioConfiguration.java
@@ -3,19 +3,13 @@ package com.zayka.zaykaAtdoor.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "twilio")
 public class TwilioConfiguration {
 	private String accountSid;
 	private String authToken;
 	private String phoneNumber;
 	
-	
-	public TwilioConfiguration(String accountSid, String authToken, String phoneNumber) {
-		super();
-		this.accountSid = accountSid;
-		this.authToken = authToken;
-		this.phoneNumber = phoneNumber;
-	}
 	public String getAccountSid() {
 		return accountSid;
 	}
